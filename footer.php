93c2d834e6f7609dab088403cfb6eb2d399b814e
Felipe Cerda <felipecerdabarra@gmail.com>
2011-12-01 00:17:11 -0300

    Version 1.0

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
 			<?php do_action( 'toolbox_credits' ); ?>
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'toolbox' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'toolbox' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'toolbox' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'toolbox' ), 'Reeder', '<a href="http://www.felipecerda.com/" rel="designer">Felipe Cerda</a>' ); ?>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'toolbox' ), 'Reader', '<a href="http://www.felipecerda.com/" rel="designer">Felipe Cerda</a>' ); ?>
 		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
